6066a37456dac53bf87b2007a2f778ef863783be
Zack Hatlen <zckhtln@icloud.com>
2022-01-18 19:02:55 +0300

    Update thankyou.php

--- plugins/woocommerce/templates/checkout/thankyou.php
+++ plugins/woocommerce/templates/checkout/thankyou.php
@@ -81,7 +81,7 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php else : ?>
 
-		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); ?></p>
 
 	<?php endif; ?>
 
